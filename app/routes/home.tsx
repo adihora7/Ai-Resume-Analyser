2f704d646d0c9b68f2d42113992c6ec4625b732e
adihora7 <aadityadihora7@gmail.com>
2026-02-07 12:05:23 -0500

    Authentication to Log in to the site

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,9 +2,19 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 
 export function meta({ }: Route.MetaArgs) {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job!" },
